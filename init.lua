7f4d95f7ddf5f5e3c8a1f7eb8267ac17f2fd0902
T0W0H <2502196137@qq.com>
2026-07-15 16:53:08 +0800

    使用老主题

--- init.lua
+++ init.lua
@@ -112,6 +112,8 @@ do
   --  Experiment for yourself to see if you like it!
   -- vim.o.relativenumber = true
 
+  vim.o.termguicolors = true
+  vim.opt.background = 'dark'
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
 
@@ -391,6 +393,34 @@ do
   --    comments = { italic = false }, -- Disable italics in comments
   --  },
   --}
+
+  vim.pack.add {
+    'https://github.com/ellisonleao/gruvbox.nvim',
+  }
+  require('gruvbox').setup {
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = 'hard', -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  }
+  vim.cmd 'colorscheme gruvbox'
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
