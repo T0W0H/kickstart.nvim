4b1db826d23df66c28a5b8f4b13a08f98df96e76
T0W0H <2502196137@qq.com>
2026-07-15 10:16:27 +0800

    加入格式化

--- init.lua
+++ init.lua
@@ -782,8 +782,9 @@ do
     format_on_save = function(bufnr)
       -- You can specify filetypes to autoformat on save here:
       local enabled_filetypes = {
-        -- lua = true,
-        -- python = true,
+        lua = true,
+        python = true,
+        cpp = true,
       }
       if enabled_filetypes[vim.bo[bufnr].filetype] then
         return { timeout_ms = 500 }
